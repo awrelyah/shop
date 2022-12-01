a0931ca69fe160c0e60e2a6f39dad8265fcb3fb6
awrelyah <aureliamihkk@gmail.com>
2022-12-01 11:25:22 +0200

    gh pages

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# shop
+# Shopping site
+
+Tried to practice e-commerde things.
+View it live [here] (https://awrelyah.github.io/shop-site/)
+
+# to do
+
+- Homepage looks kind of boring, should add something there
+- Removing items from cart
+- Possible to add one item to cart only once (?)
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,21 @@ function Header(props) {
   return (
     <div>
       <nav className="navbar">
-        <Link to="/" className="nav-link home-link"><img width="40px" src={logo} alt="logo" />Baltic Snkrs</Link>
-        <Link to="/shop" className="nav-link shop-link">Shop</Link> |{" "}
-        <Link to="/cart" className="nav-link cart-link">Cart {<span>{props.cartItems.length > 0 && props.cartItems.length}</span>}</Link>
+        <Link to="/shop-site" className="nav-link home-link">
+          <img width="40px" src={logo} alt="logo" />
+          Baltic Snkrs
+        </Link>
+        <Link to="/shop" className="nav-link shop-link">
+          Shop
+        </Link>{" "}
+        |{" "}
+        <Link to="/cart" className="nav-link cart-link">
+          Cart{" "}
+          {<span>{props.cartItems.length > 0 && props.cartItems.length}</span>}
+        </Link>
       </nav>
     </div>
   );
 }
 
-export default Header
+export default Header;
